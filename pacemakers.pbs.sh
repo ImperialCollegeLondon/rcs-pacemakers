c0f7de4cd423decd3f76418379f10d452962c4b9
Mark Woodbridge <1724545+mwoodbri@users.noreply.github.com>
2020-05-27 10:11:05 +0100

    Use DataParallel

--- pacemakers.pbs.sh
+++ pacemakers.pbs.sh
@@ -1,4 +1,4 @@
-#PBS -l walltime=01:00:00,select=1:ncpus=4:mem=24G:ngpus=1:gpu_type=P100
+#PBS -l walltime=01:00:00,select=1:ncpus=8:mem=48G:ngpus=2:gpu_type=P100
 cd $PBS_O_WORKDIR
 eval "$($HOME/anaconda3/bin/conda shell.bash hook)"
 conda run --name pacemakers python -mjupyter nbconvert pacemakers.ipynb --execute --inplace --ExecutePreprocessor.kernel_name=python3 --ExecutePreprocessor.timeout=-1
