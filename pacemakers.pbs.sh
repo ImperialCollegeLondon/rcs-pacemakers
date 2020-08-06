296d436e3436443b922a42125b8789eec9f9f9b7
Mark Woodbridge <1724545+mwoodbri@users.noreply.github.com>
2020-08-06 14:09:39 +0100

    Change environment activation

--- pacemakers.pbs.sh
+++ pacemakers.pbs.sh
@@ -1,4 +1,4 @@
 #PBS -l walltime=01:00:00,select=1:ncpus=4:mem=24G:ngpus=1:gpu_type=P100
 cd $PBS_O_WORKDIR
-eval "$($HOME/anaconda3/bin/conda shell.bash hook)"
+source ~/anaconda3/etc/profile.d/conda.sh
 conda run --name pacemakers python -mjupyter nbconvert pacemakers.ipynb --execute --inplace --ExecutePreprocessor.kernel_name=python3 --ExecutePreprocessor.timeout=-1
